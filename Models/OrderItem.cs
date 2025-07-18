c8d3fc7fc127e5152317881abd590e7fff5b76f7
Pushkar Gokhale <pushkar.gokhale@globant.com>
2025-07-18 15:35:19 +0530

    Updated OrderItems for Price and Total Price.

--- Models/OrderItem.cs
+++ Models/OrderItem.cs
@@ -17,5 +17,15 @@ namespace DotNetApiStarterKit.Models
         [Required(ErrorMessage = "Quantity is required")]
         [Range(1, int.MaxValue, ErrorMessage = "Quantity must be a positive number")]
         public int Quantity { get; set; }
+
+        [JsonPropertyName("price")]
+        [Required(ErrorMessage = "Price is required")]
+        [Range(1, double.MaxValue, ErrorMessage = "Price must be a positive number")]
+        public decimal Price { get; set; }
+
+        [JsonPropertyName("totalprice")]
+        [Required(ErrorMessage = "TotalPrice is required")]
+        [Range(1, double.MaxValue, ErrorMessage = "TotalPrice must be a positive number")]
+        public decimal TotalPrice { get; set; }
     }
 }
