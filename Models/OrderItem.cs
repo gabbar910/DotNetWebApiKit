dc7b4ad22b9581f4e4d8bb620ab4ffee62a29f82
Pushkar Gokhale <pushkar.gokhale@globant.com>
2025-08-08 10:39:59 +0530

    Migrated Orders and Orderdetails to SQLite

--- Models/OrderItem.cs
+++ Models/OrderItem.cs
@@ -1,12 +1,22 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
 
 namespace DotNetApiStarterKit.Models
 {
     public class OrderItem
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ItemId { get; set; }
+
         [JsonPropertyName("order_id")]
+        [Required]
         public int OrderId { get; set; }
+
+        [ForeignKey("OrderId")]
+        public Order? Order { get; set; }
 
         [JsonPropertyName("part_id")]
         [Required(ErrorMessage = "Part ID is required")]
@@ -20,12 +30,14 @@ namespace DotNetApiStarterKit.Models
 
         [JsonPropertyName("price")]
         [Required(ErrorMessage = "Price is required")]
-        [Range(typeof(decimal), "1", "79228162514264337593543950335", ErrorMessage = "TotalPrice must be a positive number")]
+        [Range(typeof(decimal), "0.01", "79228162514264337593543950335", ErrorMessage = "Price must be a positive number")]
+        [Precision(18, 2)]
         public decimal Price { get; set; }
 
         [JsonPropertyName("totalprice")]
         [Required(ErrorMessage = "TotalPrice is required")]
-        [Range(typeof(decimal), "1", "79228162514264337593543950335", ErrorMessage = "TotalPrice must be a positive number")]
+        [Range(typeof(decimal), "0.01", "79228162514264337593543950335", ErrorMessage = "TotalPrice must be a positive number")]
+        [Precision(18, 2)]
         public decimal TotalPrice { get; set; }
     }
 }
